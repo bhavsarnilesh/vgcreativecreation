6d664549b7d9034f5b422a0df2fe3e8e3d3cfadf
Nilesh Bhavsar <68153245+bhavsarnilesh@users.noreply.github.com>
2020-10-17 15:42:25 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/public/home/home.component';
+import { AboutComponent } from './public/about/about.component';
+import { ContactComponent } from './public/contact/contact.component';
+import { ServicesComponent } from './public/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', pathMatch: 'full', component: HomeComponent},
+  { path: 'contact', pathMatch: 'full', component: ContactComponent},
+  { path: 'about', pathMatch: 'full', component: AboutComponent},
+  { path: 'services', pathMatch: 'full', component: ServicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './public/home/home.component';
+import { ContactComponent } from './public/contact/contact.component';
+import { AboutComponent } from './public/about/about.component';
+import { ServicesComponent } from './public/services/services.component';
+import { ProjectComponent } from './public/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
